49c2c1fc51ba0756fdfe63d199437ae0fb8f57cf
sara hakimi <sarahakimi2000@yahoo.com>
2023-01-02 21:59:13 +0330

    page tamplate done without cookies

--- src/components/restaurantCard/restaurantCard.js
+++ src/components/restaurantCard/restaurantCard.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import "./style.scss"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBookmark} from "@fortawesome/free-solid-svg-icons";
 
 function RestaurantCard({restaurant}) {
     const {title, logo, id} = restaurant
-    const onClickBookmark=()=>{
+    const onClickBookmark = () => {
         console.log(id)
+
     }
     return (
         <div className="restaurant-card">
             <img src={logo} alt={title} className="restaurant-card--image"/>
-            <button onClick={onClickBookmark} type="button"><p>&hearts;</p></button><p><b>{title}</b></p>
-         </div>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button onClick={onClickBookmark} type="button" className="restaurant-card--bookmark"><FontAwesomeIcon
+                icon={faBookmark}/></button>
+            <p><b>{title}</b></p>
+        </div>
 
 
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {CookiesProvider} from 'react-cookie';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CookiesProvider>
+        <App/>
+    </CookiesProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
